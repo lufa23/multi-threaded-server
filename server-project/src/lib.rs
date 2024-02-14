1784727d1b9d5e6c36688e032c7e9c810ba7af7e
lufa23 <usuario@mind-your-business.whatever>
2024-02-14 19:27:52 +0100

    using channels

--- server-project/src/lib.rs
+++ server-project/src/lib.rs
@@ -1,8 +1,14 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool with the specified number of threads.
@@ -10,17 +16,20 @@ impl ThreadPool {
     /// Panics if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-
+        let (sender, receiver) = mpsc::channel(); 
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
             // Move ownership of the closure into the thread    
-                workers.push(Worker::new(id));
+                workers.push(
+                    Worker::new(id, Arc::clone(&receiver))
+                );
 
         
         }
 
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender}
         
     }
 
@@ -48,8 +57,13 @@ impl ThreadPool {
     }
 
     impl Worker {
-        fn new(id: usize) -> Worker {
-            let thread = thread::spawn(|| {});
+        fn new(
+            id: usize,
+            receiver: Arc<Mutex<mpsc::Receiver<Job>>>,
+        ) -> Worker {
+            let thread = thread::spawn(|| {
+                receiver;
+            });
 
             Worker {id, thread}
         }
